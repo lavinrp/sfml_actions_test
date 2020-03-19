0e4f2ce52ae4676218fa77171930d4f7d727c8c7
Ryan Lavin <lavinrp@gmail.com>
2020-03-19 16:07:04 -0700

    add SFML example

---
+++ sfml_actions_test.cpp
@@ -0,0 +1,33 @@
+﻿// sfml_actions_test.cpp : Defines the entry point for the application.
+//
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::CircleShape circle(100);
+    circle.setPosition(200, 150);
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Draw the sprite
+        window.draw(circle);
+        // Update the window
+        window.display();
+        window.close();
+    }
+    return EXIT_SUCCESS;
+}
